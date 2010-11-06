b1ea8d0de3b016bcec8201cd5fa17e03414541ae
Jeffrey Ness <jness@flip-edesign>
2010-11-06 12:48:11 -0500

    Added donate button to About page

--- sarGraphs_1.1.0/about.php
+++ sarGraphs_1.1.0/about.php
@@ -14,7 +14,16 @@ include "includes/slim.php";
 	<b>Source:</b> <a href='https://github.com/jness/sarGraphs'><font color='blue' size='2'><u>Availble at Github</u></font></a>
 	<br><br>
 	sarGraphs is a Open Source project written in PHP and Bash, <a href='http://jpgraph.net/'><u><font color='blue'>JpGraph</font></u></a> is the PHP library used to create graphs from the <a href='http://sebastien.godard.pagesperso-orange.fr/'><u><font color='blue'>sysstat</font></u></a> data.
-	</font></td>
+	</font> <br><br>
+
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="AZ72E33D54RQJ">
+<input type="image" src="https://www.paypal.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
+
+    </td>
   </tr>
 </table>
 <?php
