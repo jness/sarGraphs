69b43641748c17c5c8467dc44d00f3c02bc86d3e
Jeffrey Ness <jness@flip-edesign>
2010-11-11 17:31:10 -0600

    Altering the default page layout, and some README information

--- sarGraphs_1.1/index.php
+++ sarGraphs_1.1/index.php
@@ -28,14 +28,15 @@ include "includes/head.php";
 
 		?>
 
-		<a href='history.php?type=cpu'><img border=0 src=graphs/cpu-current.jpg></a>
+		 <a href='history.php?type=cpu'><img border=0 src=graphs/cpu-current.jpg></a>
+		 <br>
 		 <a href='history.php?type=memory'><img border=0 src=graphs/memory-current.jpg></a>
-		 <br>
-		 <a href='history.php?type=network'><img border=0 src=graphs/network-current.jpg></a>
-		 <br>
 		 <a href='history.php?type=swap'><img border=0 src=graphs/swap-current.jpg></a>
+		 <br>
 		 <a href='history.php?type=io'><img border=0 src=graphs/io-current.jpg></a>
 		 <a href='history.php?type=load'><img border=0 src=graphs/load-current.jpg></a>
+		 <br>
+		 <a href='history.php?type=network'><img border=0 src=graphs/network-current.jpg></a>
 
 		<?php
 
