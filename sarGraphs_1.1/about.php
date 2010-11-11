e7d47f5532fb27723f768887abd080f90c0617db
Jeffrey Ness <jness@flip-edesign>
2010-11-11 17:33:46 -0600

    removing Donate button

--- sarGraphs_1.1/about.php
+++ sarGraphs_1.1/about.php
@@ -15,13 +15,7 @@ include "includes/slim.php";
 	<br><br>
 	sarGraphs is a Open Source project written in PHP and Bash, <a href='http://pchart.sourceforge.net'><u><font color='blue'>pChart</font></u></a> is the PHP library used to create graphs from the <a href='http://sebastien.godard.pagesperso-orange.fr/'><u><font color='blue'>sysstat</font></u></a> data.
 	</font> <br><br>
-
-<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
-<input type="hidden" name="cmd" value="_s-xclick">
-<input type="hidden" name="hosted_button_id" value="AZ72E33D54RQJ">
-<input type="image" src="https://www.paypal.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-<img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
-</form>
+	
 
     </td>
   </tr>
