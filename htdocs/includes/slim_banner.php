ac58bf840c94e99075c5cfdad266c1a1f400903f
Jeffrey Ness <jness@flip-edesign.com>
2010-11-22 12:35:52 -0600

    slim it down

--- htdocs/includes/slim_banner.php
+++ htdocs/includes/slim_banner.php
@@ -1,5 +1,5 @@
 </table>
-<table width="950"  border="0" align="center" cellpadding="10">
+<table width="950"  border="0" align="center" cellpadding="0">
   <tr>
     <td><center><h1><a href='index.php'><font color='#666666' face='Arial, Helvetica, sans-serif'>sarGraphs</font></a></h1>
         </center>
