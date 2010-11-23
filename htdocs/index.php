75d2b6d85ed9655efbf4024ba5d11264e7f999a7
Jeffrey Ness <jness@flip-edesign.com>
2010-11-23 17:40:46 -0600

    Adding Y and X Axis lables to graphs

--- htdocs/index.php
+++ htdocs/index.php
@@ -31,11 +31,9 @@ include "includes/banner.php";
 
 		 <a href='history.php?type=cpu'><img border=0 src=graphs/cpu-current.jpg></a>
 		 <br>
-		 <a href='history.php?type=memory'><img border=0 src=graphs/memory-current.jpg></a>
-		 <a href='history.php?type=swap'><img border=0 src=graphs/swap-current.jpg></a>
+		 <a href='history.php?type=memory'><img border=0 src=graphs/memory-current.jpg></a><a href='history.php?type=swap'><img border=0 src=graphs/swap-current.jpg></a>
 		 <br>
-		 <a href='history.php?type=io'><img border=0 src=graphs/io-current.jpg></a>
-		 <a href='history.php?type=load'><img border=0 src=graphs/load-current.jpg></a>
+		 <a href='history.php?type=io'><img border=0 src=graphs/io-current.jpg></a><a href='history.php?type=load'><img border=0 src=graphs/load-current.jpg></a>
 		 <br>
 		 <a href='history.php?type=network'><img border=0 src=graphs/network-current.jpg></a>
 
